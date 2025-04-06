9806f881e32a8cb980d3d44a4be6da7e3936f4d8
Ajay Chaudhary <ajaych2822@gmail.com>
2025-04-06 10:30:34 +0530

    Updated code

--- predict.py
+++ predict.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b77c7d3c7c752aedc3e34253f70bc6a9b67433e6ea0ce926f3c16904237bfeba
-size 3734
+oid sha256:d96e02fb28dcd95032e4b98ea86cfe95d312bfdee587f89d47821d75b732c0fe
+size 2430
--- train.py
+++ train.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e890516c9873a2b737f2efa21d1bba52d017ea4eca9e04cdcf6246ae9dcf7adb
-size 5647
+oid sha256:fb284dfc3eb9c11add7d24475d0f1bc0fe4c74d5711a829bbd3c1ced9736978f
+size 510
